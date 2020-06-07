f05e7178e76a369827ebc993000605ac144e8820
abhinav <abhinav.eerlapati@mckesson.com>
2020-06-07 12:40:08 -0700

    commit for pipeline.yml

--- src/main/java/com/training/TodoAppSpringBoot/AppConfiguration.java
+++ src/main/java/com/training/TodoAppSpringBoot/AppConfiguration.java
@@ -1,9 +1,13 @@
 package com.training.TodoAppSpringBoot;
+import java.sql.SQLException;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.h2.*;
 
 
 @Configuration
@@ -17,5 +21,16 @@ public class AppConfiguration {
         messageSource.setCacheSeconds(10); //reload messages every 10 seconds
         return messageSource;
     }
+	
+	
+	 @Bean
+	    ServletRegistrationBean h2servletRegistration(){
+	        ServletRegistrationBean registration = new ServletRegistrationBean( new org.h2.server.web.WebServlet());
+	        registration.addUrlMappings("/h2-console/*");
+	        registration.addInitParameter("webAllowOthers", "true");
+	        registration.addInitParameter("webPort", "7777");// <-- the port your wish goes here
+
+	        return registration;
+	    }
 	
 }
